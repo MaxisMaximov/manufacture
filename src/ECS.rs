b325496f736a057f6d2d109e3c23f6cf83f7f274
Maxis Scott Engie Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2024-09-29 14:50:44 +0000

    = Added a `gmObj` struct

--- src/ECS.rs
+++ src/ECS.rs
@@ -2,12 +2,19 @@ use super::*;
 
 pub trait gmComp{}
 
+pub struct gmObj{
+    ID: u16
+}
+
 mod tests{
     use super::*;
 
     pub fn main(){
         let mut hpVec:Vec<gmComp_Health> = Vec::new();
         hpVec.push(gmComp_Health{val: 100});
+
+        let mut objVec: Vec<gmObj> = Vec::new();
+        objVec.push(gmObj{ID: 0});
     }
 
     struct gmComp_Health{
