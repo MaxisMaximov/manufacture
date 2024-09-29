8de2c928a5912f8730a1e6e75a457d8895242ef9
Maxis Scott Engie Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2024-09-29 15:08:05 +0000

    = Added a `gmWorld` struct

--- src/ECS.rs
+++ src/ECS.rs
@@ -6,19 +6,34 @@ pub struct gmObj{
     ID: u16
 }
 
+pub struct gmWorld{
+    gmObjs: Vec<gmObj>,
+    hpVec: Vec<tests::gmComp_Health>,
+    posVec: Vec<tests::gmComp_Pos>
+}
+
 mod tests{
     use super::*;
 
     pub fn main(){
-        let mut hpVec:Vec<gmComp_Health> = Vec::new();
-        hpVec.push(gmComp_Health{val: 100});
+        let mut world = gmWorld{
+            gmObjs: Vec::new(),
+            hpVec: Vec::new(),
+            posVec: Vec::new(),
+        };
 
-        let mut objVec: Vec<gmObj> = Vec::new();
-        objVec.push(gmObj{ID: 0});
+        world.hpVec.push(gmComp_Health{val: 100});
+        world.posVec.push(gmComp_Pos{x: 0, y: 0});
     }
 
-    struct gmComp_Health{
+    pub struct gmComp_Health{
         val: u16
     }
     impl gmComp for gmComp_Health{}
+
+    pub struct gmComp_Pos{
+        x: usize,
+        y: usize
+    }
+    impl gmComp for gmComp_Pos{}
 }
