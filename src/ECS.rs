d12b5bd33cd14516b5dabad4c9568c18dc56bc32
Maxis Scott Engie Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2024-09-29 14:47:59 +0000

    = Added a `gmComp` trait

--- src/ECS.rs
+++ src/ECS.rs
@@ -1,7 +1,17 @@
 use super::*;
 
+pub trait gmComp{}
+
 mod tests{
+    use super::*;
+
     pub fn main(){
-        
+        let mut hpVec:Vec<gmComp_Health> = Vec::new();
+        hpVec.push(gmComp_Health{val: 100});
     }
+
+    struct gmComp_Health{
+        val: u16
+    }
+    impl gmComp for gmComp_Health{}
 }
