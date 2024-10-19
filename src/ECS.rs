3d7414da32f682f7197d13353190e15641c490cb
Maxis Scott Engie Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2024-10-19 14:22:33 +0000

    = Added `gmStorageDrop` trait

--- src/ECS.rs
+++ src/ECS.rs
@@ -119,9 +119,17 @@ impl gmDispatcher{
 }
 
 pub trait gmStorage<T>: Any{
-    fn new() -> Self;
+    fn new() -> Self where Self: Sized;
     fn insert(&mut self, IN_id: gmID, IN_item: T);
     fn remove(&mut self, IN_id: gmID) -> Option<T>;
+}
+pub trait gmStorageDrop: Any{
+    fn drop(&mut self, IN_id: gmID);
+}
+impl<T: 'static> gmStorageDrop for dyn gmStorage<T>{
+    fn drop(&mut self, IN_id: gmID) {
+        self.remove(IN_id);
+    }
 }
 
 #[derive(Clone, Copy)]
