5b6e664856a3ca58767d03654d043f2d120ee5e7
Maxis Scott Engie Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2024-10-23 09:18:28 +0000

    = Added `new` function to `gmGenIndex`

--- src/ECS.rs
+++ src/ECS.rs
@@ -132,6 +132,15 @@ pub struct gmGenIndex<T: Sized>{
     pub gen: gmGen,
     pub entry: Option<T>
 }
+impl<T: Sized> gmGenIndex<T>{
+    pub fn new(IN_id: gmID, IN_entry: Option<T>) -> Self{
+        Self{
+            id: IN_id,
+            gen: 0,
+            entry: IN_entry,
+        }
+    }
+}
 
 pub struct gmObjStorage{
     pub gmObjMap: HashMap<gmID, gmObj>,
@@ -144,9 +153,11 @@ impl gmObjStorage{
             nextFree: BTreeMap::new()
         }
     }
+
     pub fn insert(&mut self, IN_id: gmID){
-        self.gmObjMap.entry(IN_id).or_insert(gmObj{id: IN_id, gen: 0, entry: Some(())});
+        self.gmObjMap.entry(IN_id).or_insert(gmObj::new(IN_id, Some(())));
     }
+
     pub fn insertNextFree(&mut self) -> gmID{
         let w_nextIndex: gmID = if self.nextFree.len() == 0{
             // If there's no nextFree, length is always the next index
@@ -156,14 +167,11 @@ impl gmObjStorage{
             self.nextFree.pop_first().unwrap().0
         };
 
-        self.gmObjMap.insert(w_nextIndex, gmObj{
-            id: w_nextIndex,
-            gen: 0,
-            entry: Some(()),
-        });
+        self.gmObjMap.insert(w_nextIndex, gmObj::new(w_nextIndex, Some(())));
 
         return w_nextIndex
     }
+
     pub fn remove(&mut self, IN_id: gmID){
         self.gmObjMap.remove(&IN_id);
         self.nextFree.insert(IN_id, ());
