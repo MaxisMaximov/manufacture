fab10fc032ce82236f14a9b4d3f77241d674375e
Maxis Scott Engie Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2024-11-05 14:28:27 +0000

    = Added Position and Velocity components

--- src/manufacture/components/mod.rs
+++ src/manufacture/components/mod.rs
@@ -10,3 +10,27 @@ impl gmComp for comp_HP{
         "comp_HP"
     }
 }
+
+pub struct comp_Pos{
+    x: usize,
+    y: usize
+}
+impl gmComp for comp_Pos {
+    type COMP_STORAGE = denseVecStorage<Self>;
+
+    fn COMP_ID() -> &'static str {
+        "comp_Pos"
+    }
+}
+
+pub struct comp_Vel{
+    x: usize,
+    y: usize
+}
+impl gmComp for comp_Vel {
+    type COMP_STORAGE = denseVecStorage<Self>;
+
+    fn COMP_ID() -> &'static str {
+        "comp_Vel"
+    }
+}
