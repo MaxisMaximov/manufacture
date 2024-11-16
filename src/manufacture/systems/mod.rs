d100d4ce32266846003d936214bef773a11f820a
Maxis Scott Engie Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2024-11-16 13:32:07 +0000

    = Added TileChunkUpdate system It was SUPER painful Check the latest comment in #22 for calculations

--- src/manufacture/systems/mod.rs
+++ src/manufacture/systems/mod.rs
@@ -134,9 +134,9 @@ impl<'a> gmSystem<'a> for sys_PTileChange{
         "sys_PTileChange"
     }
 
-    fn execute(&mut self, mut IN_data: Self::sysData) {
+    fn execute(&mut self, IN_data: Self::sysData) {
         let w_PCoords = IN_data.comp_Pos.inner.get(*IN_data.res_PID.inner.res.get(&1).unwrap());
-        let w_oldTile = IN_data.res_World.inner.getTileMut((w_PCoords.x, w_PCoords.y)).unwrap().mat;
+        let w_oldTile = IN_data.res_World.inner.getTile((w_PCoords.x, w_PCoords.y)).unwrap().mat;
         let mut w_tileChangeEvents = IN_data.res_Events.inner.getEventWriter::<event_TileChange>();
 
         match IN_data.res_PInput.inner.res.code{
@@ -163,7 +163,7 @@ impl<'a> gmSystem<'a> for sys_PTileChange{
     }
 }
 pub struct sysData_PTileChange<'a>{
-    res_World: FetchResMut<'a, res_GridWorld>,
+    res_World: FetchRes<'a, res_GridWorld>,
     res_PInput: FetchRes<'a, res_PInput>,
     res_PID: FetchRes<'a, res_PID>,
     res_Events: FetchRes<'a, res_Events>,
@@ -172,11 +172,73 @@ pub struct sysData_PTileChange<'a>{
 impl<'a> gmSystemData<'a> for sysData_PTileChange<'a>{
     fn fetch(IN_world: &'a mut gmWorld) -> Self {
         Self{
-            res_World: IN_world.fetchResMut::<res_GridWorld>(),
+            res_World: IN_world.fetchRes::<res_GridWorld>(),
             res_PInput: IN_world.fetchRes::<res_PInput>(),
             res_PID: IN_world.fetchRes::<res_PID>(),
             res_Events: IN_world.fetchRes::<res_Events>(),
             comp_Pos: IN_world.fetch::<comp_Pos>()
+        }
+    }
+}
+
+pub struct sys_TileChunkUpdate{}
+impl<'a> gmSystem<'a> for sys_TileChunkUpdate{
+    type sysData = sysData_TileChunkUpdate<'a>;
+
+    fn new() -> Self {
+        Self{}
+    }
+
+    fn SYS_ID() -> &'static str {
+        "sys_TileChunkUpdate"
+    }
+
+    fn execute(&mut self, mut IN_data: Self::sysData) {
+        let w_Events_TileChange = IN_data.res_Events.inner.getEventReader::<event_TileChange>();
+        let w_Events_BatchTileChange = IN_data.res_Events.inner.getEventReader::<event_BatchTileChange>();
+
+        for EVENT in w_Events_TileChange.inner.iter(){
+            if let Some(TILE) = IN_data.res_World.inner.getTileMut(EVENT.coords){
+                TILE.mat = EVENT.newTile
+            }
+        }
+
+        for EVENT in w_Events_BatchTileChange.inner.iter(){
+
+            for COORDS in IN_data.res_World.inner.getChunkRange(EVENT.from, EVENT.to).iter(){
+
+                if let Some(w_chunk) = IN_data.res_World.inner.getChunkMut(*COORDS){
+                    use std::cmp::{max, min};
+                    // This is a mess but I calculated EVERYTHING for 2 hours straight for it to work
+                    // Basically constraints iterator to chunk's boundaries and local coordinates
+
+                    // Check latest comment in #22 for calculations
+
+                    // Listen chunk iterators are better than accessing constantly from world level
+                    for XPOS in
+                        (max(EVENT.from.0, COORDS.0*6))/6 // FROM: Whichever is closer to 0
+                        ..(min(EVENT.to.0+1, COORDS.0*6+6))/6 // TO: Whichever is closer to 0
+                        {
+                            for YPOS in // Same stuff
+                                (max(EVENT.from.1, COORDS.1*6))/COORDS.0*6
+                                ..(min(EVENT.to.1+1, COORDS.1*6+6))/COORDS.1*6{
+                                    w_chunk[(XPOS, YPOS)].mat = EVENT.newTile
+                                }
+                    }
+                }
+            }
+        }
+    }
+}
+pub struct sysData_TileChunkUpdate<'a>{
+    pub res_Events: FetchRes<'a, res_Events>,
+    pub res_World: FetchResMut<'a, res_GridWorld>
+}
+impl<'a> gmSystemData<'a> for sysData_TileChunkUpdate<'a>{
+    fn fetch(IN_world: &'a mut gmWorld) -> Self {
+        Self{
+            res_Events: IN_world.fetchRes::<res_Events>(),
+            res_World: IN_world.fetchResMut::<res_GridWorld>(),
         }
     }
 }
