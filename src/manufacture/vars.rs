ebba81e823d51536cd89339754d88b60f4a2c0c0
Maxis Scott Engie Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2024-11-20 12:21:12 +0000

    = Added some Renderer vars

--- src/manufacture/vars.rs
+++ src/manufacture/vars.rs
@@ -10,6 +10,14 @@ pub const WORLD_Y_MAX: isize = 5;
 pub const CHUNK_X: isize = 8;
 pub const CHUNK_Y: isize = 8;
 
+pub const RENDER_VIEWPORT_X_MIN: isize = -5;
+pub const RENDER_VIEWPORT_X_MAX: isize = 5;
+pub const RENDER_VIEWPORT_Y_MIN: isize = -5;
+pub const RENDER_VIEWPORT_Y_MAX: isize = 5;
+
+// To still render objects if they're "technically" in view
+pub const RENDER_MARGIN: isize = 4;
+
 pub type Vector2 = (isize, isize);
 
 pub struct FetchEvent<'a, T: gmEvent>{
