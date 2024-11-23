d5b97e5041703605f7f777c38281bd1196d81536
Maxis Scott Engie Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2024-11-23 11:41:08 +0000

    = Added Buffer size vars

--- src/manufacture/vars.rs
+++ src/manufacture/vars.rs
@@ -16,6 +16,11 @@ pub const RENDER_VIEWPORT_X_MAX: isize = 5;
 pub const RENDER_VIEWPORT_Y_MIN: isize = -5;
 pub const RENDER_VIEWPORT_Y_MAX: isize = 5;
 
+// Yeah smaller than the old buffer, realized I don't use most of it anyway lol
+// Yet
+pub const RENDER_BUFFER_X: usize = 20;
+pub const RENDER_BUFFER_Y: usize = 20;
+
 // To still render objects if they're "technically" in view
 pub const RENDER_MARGIN: isize = 4;
 
