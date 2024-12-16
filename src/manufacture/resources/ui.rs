115bd1fb74af41b03f019f5a8a3741357805e5f6
Maxis Scott Engie Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2024-12-16 13:47:27 +0000

    = Added Depth parameter to the Tree

--- src/manufacture/resources/ui.rs
+++ src/manufacture/resources/ui.rs
@@ -9,9 +9,12 @@ pub struct UI_element{
 }
 
 pub struct NodeTree<T>{
-    root: Node<T>
+    root: Node<T>,
+    maxDepth: u16 // The tree should NOT consist of 32k layers, if it does, you're doing something horribly wrong
 }
 pub struct Node<T>{
     val: T,
+    depth: u16,
+    _maxDepth: u16, // Yeah, each node carries the tree's max depth to not do super long recall chain
     nodes: Vec<Node<T>>
 }
