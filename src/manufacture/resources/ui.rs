9822eb4304ff258f6a47b605bd9d237cc4342462
Maxis Scott Engie Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2024-12-17 14:55:56 +0000

    = Minor fix

--- src/manufacture/resources/ui.rs
+++ src/manufacture/resources/ui.rs
@@ -41,9 +41,12 @@ impl<T: Clone> Node<T>{
         }
     }
 
-    pub fn addNode(&mut self, IN_val: T) -> usize{
+    pub fn addNode(&mut self, IN_val: T) -> Result<usize, ()>{
+        if self.depth >= self.maxDepth {
+            return Err(())
+        }
         self.nodes.push(Self::new(IN_val, self.depth + 1, self.maxDepth));
-        self.nodes.len() - 1
+        Ok(self.nodes.len() - 1)
     }
     pub fn removeNode(&mut self, IN_id: usize) -> Node<T>{
         self.nodes.remove(IN_id)
