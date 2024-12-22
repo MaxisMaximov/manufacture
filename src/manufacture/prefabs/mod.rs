c3d65435744c5e612c8b3a9311dc64c423671f3d
Maxis Scott Engie Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2024-12-22 10:46:31 +0000

    = Added Chunk prefab

--- src/manufacture/prefabs/mod.rs
+++ src/manufacture/prefabs/mod.rs
@@ -1,13 +1,12 @@
 use super::*;
 
 use components::*;
-use types::StyleSet;
+use types::{StyleSet, Vector2};
+use vars::*;
 
 use super::gmPrefab;
 
-pub struct prefab_Player{
-
-}
+pub struct prefab_Player;
 impl gmPrefab for prefab_Player{
     fn spawn(&self, IN_builder: gmObjBuilder) {
         IN_builder
@@ -16,5 +15,17 @@ impl gmPrefab for prefab_Player{
             .addComp(comp_Pos{x: 0, y: 0})
             .addComp(comp_Vel{x: 0, y: 0})
             .addComp(comp_Sprite{ sizeX: 1, sizeY: 1, sprite: vec![StyleSet{ ch: 'P', fg: Color::White, bg: Color::Cyan }], zDepth: 1 });
+    }
+}
+
+pub struct prefab_GridWorldChunk{
+   chunk: Vector2
+}
+impl gmPrefab for prefab_GridWorldChunk{
+    fn spawn(&self, IN_builder: gmObjBuilder) {
+        IN_builder
+            .addComp(comp_Pos{ x: self.chunk.0 * CHUNK_X, y: self.chunk.1 * CHUNK_Y })
+            .addComp(comp_TileTerrainChunk{ chunk: self.chunk })
+            .addComp(comp_Sprite{ sizeX: CHUNK_X as usize, sizeY: CHUNK_Y as usize, sprite: vec![StyleSet{ ch: ' ', fg: Color::White, bg: Color::Black }], zDepth: 0 });
     }
 }
