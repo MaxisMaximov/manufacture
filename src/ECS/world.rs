664aa55be416d09f17690dc858aec3a04316243d
Maxis Scott Engie Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2024-12-24 13:07:03 +0000

    = Adjusted `commandsExec` function to not yell constantly Will fix it properly later

--- src/ECS/world.rs
+++ src/ECS/world.rs
@@ -113,6 +113,15 @@ impl gmWorld{
     }
 
     pub fn commandsExec(&mut self){
+        loop{
+            let idkfa = self.commands.as_ref().borrow_mut().pop();
+            match idkfa{
+                Some(COMMAND) => COMMAND.execute(self),
+                None => break,
+            }
+        }
+    }
+
     pub fn fetchCommandWriter<'a>(&'a self) -> FetchMut<'a, gmWorld_CMDQUEUE>{
         FetchMut{
             data: RefMut::map(self.commands.as_ref().borrow_mut(), |idkfa| {idkfa}),
