9238a3de8ae51dfaa5c4c4326be9e47ea6b48c73
Maxis Scott Engie Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2024-12-29 13:02:38 +0000

    = Added `RangeOffset` trait to Utils

--- src/manufacture/utils.rs
+++ src/manufacture/utils.rs
@@ -2,6 +2,7 @@ use types::Vector2;
 use vars::*;
 
 use super::*;
+
 pub fn util_coordConvert(IN_coords: Vector2) -> (Vector2, Vector2){
     let mut w_chunkCoords: Vector2 = (IN_coords.0 / CHUNK_X, IN_coords.1 / CHUNK_Y);
     let w_tileCoords: Vector2 = (IN_coords.0 % CHUNK_X, IN_coords.1 % CHUNK_Y);
@@ -15,4 +16,20 @@ pub fn util_coordConvert(IN_coords: Vector2) -> (Vector2, Vector2){
     }
 
     (w_chunkCoords, w_tileCoords)
+}
+
+pub mod rangeStuff{
+    use std::ops::{Add, Range};
+
+    pub trait RangeOffset<U>{
+        fn offset(self, IN_offset: U) -> Self;
+    }
+    impl<T, U> RangeOffset<U> for Range<T> where T: Add<U, Output = T>, U: Copy{
+        fn offset(self, IN_offset: U) -> Self{
+            Range{
+                start: self.start + IN_offset,
+                end: self.end + IN_offset,
+            }
+        }
+    }
 }
