f485227ae16969602fc85be95a8292f82f5e2a7a
Maxis Scott "Engie" Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2025-01-05 13:39:11 +0100

    UI>> Moved to it's own folder and Specials into their own file

--- src/manufacture/resources/ui/mod.rs
+++ src/manufacture/resources/ui/mod.rs
@@ -1,5 +1,7 @@
 use super::*;
 
+pub mod specials;
+
 pub struct UI_element{
     pub content: fn(&res_UIData) -> String,
     pub style: UI_style
@@ -47,30 +49,4 @@ pub enum UI_border{
     none,
     singleChar(char),
     fancy
-}
-
-pub mod specials{
-    pub fn progressBar(IN_length: usize, IN_val: usize, IN_maxVal: usize) -> String{
-        let w_filledIn = (IN_length * ((IN_val * 100) / IN_maxVal)) / 100; // (LEN * PERCENT) / 100
-        " ".repeat(IN_length) // Empty progressbar
-            .replacen(" ", "█", w_filledIn) // Replace
-    }
-    pub fn percent(IN_val: usize, IN_maxVal: usize) -> String{
-        format!("{}%", ((IN_val * 100) / IN_maxVal))
-    }
-    pub fn list(IN_items: &[String], IN_maxWidth: usize, IN_vertical: bool) -> String{
-        let mut OUT_str = String::new();
-
-        for ITEM in IN_items.iter(){
-            OUT_str.push_str(&ITEM[0..IN_maxWidth]);
-            if IN_vertical{
-                OUT_str.push('\n'); // Newline append
-            }
-            else{
-                OUT_str.push_str(" | ");
-            }
-        }
-
-        OUT_str
-    }
 }
---
+++ src/manufacture/resources/ui/specials.rs
@@ -0,0 +1,24 @@
+
+pub fn progressBar(IN_length: usize, IN_val: usize, IN_maxVal: usize) -> String{
+    let w_filledIn = (IN_length * ((IN_val * 100) / IN_maxVal)) / 100; // (LEN * PERCENT) / 100
+    " ".repeat(IN_length) // Empty progressbar
+        .replacen(" ", "█", w_filledIn) // Replace
+}
+pub fn percent(IN_val: usize, IN_maxVal: usize) -> String{
+    format!("{}%", ((IN_val * 100) / IN_maxVal))
+}
+pub fn list(IN_items: &[String], IN_maxWidth: usize, IN_vertical: bool) -> String{
+    let mut OUT_str = String::new();
+
+    for ITEM in IN_items.iter(){
+        OUT_str.push_str(&ITEM[0..IN_maxWidth]);
+        if IN_vertical{
+            OUT_str.push('\n'); // Newline append
+        }
+        else{
+            OUT_str.push_str(" | ");
+        }
+    }
+
+    OUT_str
+}
