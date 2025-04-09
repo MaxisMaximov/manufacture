d9c2e50a071291eeb73fdd4be27f8b33654be7e1
Maxis Scott Engie Maximov Jr. <61377417+MaxisMaximov@users.noreply.github.com>
2025-04-09 09:21:40 +0000

    Small reorganization of UI structs

--- src/manufacture/resources/ui/mod.rs
+++ src/manufacture/resources/ui/mod.rs
@@ -1,60 +1,20 @@
 use super::*;
 
-pub mod specials;
+mod specials;
+pub use specials::Special;
 
-pub struct UIElement{
-    pub tag: UITag,
-    pub style: UIStyle
+pub struct UINode{
+    pub tag: Tag,
+    pub calculated: Calculated
 }
 
-pub struct UICalculated{
-    pub offset: (usize, usize),
+pub struct Calculated{
+    pub position: (usize, usize),
     pub box_bounding: (usize, usize)
 }
 
-pub enum UITag{
+pub enum Tag{
     None,
-    /// You *can* add subnodes to this one  
-    /// But it's recommended not to as it can break stuff
     Text(String),
-    /// # WARNING
-    /// The node **MUST NOT HAVE** subnodes  
-    /// Specials fully reconstruct their nodes
-    Special(Box<dyn specials::UISpecial>)
-}
-impl UITag{
-    pub fn take(&mut self) -> UITag{
-        std::mem::replace(self, UITag::None)
-    }
-    /// Yes, this is stupid  
-    /// But I like it lol
-    pub fn giveBack(&mut self, IN_val: Self){
-        let _ = std::mem::replace(self, IN_val);
-    }
-}
-
-pub struct UIStyle{
-    pub position: UIPos,
-    pub size: UISize,
-    pub fg: Color,
-    pub bg: Color,
-    pub border: UIBorder,
-    pub display: UIDisplay
-}
-
-pub enum UIPos{
-    Static,
-    Abs(Vector2),
-    Rel(Vector2)
-}
-pub enum UISize{
-    Abs((usize, usize)),
-    Frac((usize, usize))
-}
-pub enum UIBorder{
-    None,
-    SingleChar(char),
-    Fancy
-}pub enum UIDisplay{
-    Float
+    Special(Box<dyn specials::Special>)
 }
